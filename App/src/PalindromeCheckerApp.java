4c8ecbc45423e06735dda4ce024e7e87affeef7e
dshxh <ds8354@srmist.edu.in>
2026-02-19 10:14:45 +0530

    completed UC2-gunjan

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,7 +1,14 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialised successfully.");
+    public static void main (String[] args){
+        String original = "madam";
+
+        String reversed = new StringBuilder(original).reverse().toString();
+
+        if (original.equalsIgnoreCase(reversed)) {
+            System.out.println("The string '" + original + "' is a palindrome.");
+        } else {
+            System.out.println("The string '" + original + "' is NOT a palindrome.");
+        }
+
     }
 }
