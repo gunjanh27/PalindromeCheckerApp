4d86bcfdff0ed90460b3ec139e45926a249a71f2
gunjanh27 <gh3988@srmist.edu.in>
2026-02-26 11:09:57 +0530

    C9-Palindrome completed gunjan

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,22 +1,19 @@
-import java.util.LinkedList;
 public class PalindromeCheckerApp {
     public static void main (String[] args){
-        String input = "level";
+        String input = "madam";
+        boolean result = check(input, 0, input.length() - 1);
+
         System.out.println("Input : " + input);
-        LinkedList<Character> list = new LinkedList<>();
-        for (char c : input.toCharArray()) {
-            list.add(c);
+        System.out.println("Is Palindrome? : " + result);
+    }
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) {
+            return true;
         }
-        boolean isPalindrome = true;
-        while (list.size() > 1) {
-            Character first = list.removeFirst();
-            Character last = list.removeLast();
-            if (!first.equals(last)) {
-                isPalindrome = false;
-                break;
-            }
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
         }
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        return check(s, start + 1, end - 1);
     }
 }
 
